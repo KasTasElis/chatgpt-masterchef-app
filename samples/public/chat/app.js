d2eda4159aa9090ea3ed081eaeec3ff155db2461
EliHimself <tamosauskas09@gmail.com>
2023-10-14 19:27:55 +0300

    feat: implement custom chat GPT

--- samples/public/chat/app.js
+++ samples/public/chat/app.js
@@ -1,15 +1,56 @@
-const OPENAI_KEY = "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";
-const price = 0.0002/1000;
+const URL = "http://localhost:3000/api/chat";
+const USER_NAME = "Eli";
 
-const messages = [];
-let totalTokens = 0;
+const messages = [{ role: "system", content: "Pretend you are Elon Musk." }];
 
 async function sendChat() {
-    const prompt = document.querySelector("#prompt").value;
-    document.querySelector("#prompt").value = "";
-   
-    // TODO make query and parse results
+  const prompt = document.querySelector("#prompt");
 
-    document.querySelector("#prompt").value = "";
+  // trigger loading state
+  const submitButton = document.querySelector("button");
+  submitButton.disabled = true;
+  submitButton.innerText = "Loading...";
+  prompt.disabled = true;
+
+  const userMessage = { role: "user", content: prompt.value };
+  messages.push(userMessage);
+
+  // push user message to the ul
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  const b = document.createElement("b");
+  b.innerText = `${USER_NAME}: ${prompt.value}`;
+  li.appendChild(b);
+  ul.appendChild(li);
+
+  // reset field
+  document.querySelector("#prompt").value = "";
+
+  try {
+    const respData = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        messages,
+      }),
+    });
+
+    if (respData.status !== 200) throw new Error("Something went wrong");
+
+    const message = await respData.json();
+    messages.push(message);
+
+    // push message to the UL
+    ul.appendChild(document.createElement("li")).innerText = message.content;
+  } catch (error) {
+    alert("Woops, something went wrong, please try again later.");
+    console.error("Something went wrong: ", error);
+  } finally {
     document.querySelector("input").focus();
+    submitButton.disabled = false;
+    prompt.disabled = false;
+    submitButton.innerText = "Send";
+  }
 }
--- samples/server.js
+++ samples/server.js
@@ -1,33 +1,50 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-
-const OPENAI_KEY = "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";
-
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
 const app = express();
 
 app.use(express.json()); // parse JSON requests
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+const OPENAI_KEY = process.env.OPENAI_KEY;
+const URL = "https://api.openai.com/v1/chat/completions";
 
-app.post('/api/chat', async (req, res) => {
-  
+app.post("/api/chat", async (req, res) => {
+  const messages = req.body.messages;
+
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPENAI_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        temperature: 0,
+        messages,
+      }),
+    });
+
+    const data = await response.json();
+    const message = data.choices[0].message;
+
+    res.json(message);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 });
 
-app.post('/api/general', async (req, res) => {
-  
-});
+app.post("/api/general", async (req, res) => {});
 
-app.post('/api/image', async (req, res) => {
-  
-})
+app.post("/api/image", async (req, res) => {});
 
-app.post('/api/recipe', async (req, res) => {
-  
-});
+app.post("/api/recipe", async (req, res) => {});
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
